a2a6db907c1c79aa532587b95764a9044fe58e31
Saumyasharma974 <sharmasaumya2464@gmail.com>
2025-11-08 22:28:42 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import Home from './pages/Home'
- import ProjectsSection from './pages/AboutUsPage'
-// import ContactSection from './pages/ContactForm'
-// import StoryboardSection from './pages/Services'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import ProjectsSection from "./pages/AboutUsPage";
+import ContactSection from "./pages/ContactForm"
+import Navbar from './components/Navbar'
+import StoryboardSection from "./pages/Services";
+// import StoryboardSection from "./pages/Services"
 
 function App() {
   return (
-    <div>
-      <Home />
- 
-    </div>
-  )
+   
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<ProjectsSection />} />
+        <Route path="/contact" element={<ContactSection />} />
+        <Route path="/services" element={<StoryboardSection />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Navbar from '../components/Navbar'
+
 import HeroSection from '../components/Herosection'
 
 function Home() {
   return (
     <div>
-        <Navbar/>
+      
         <HeroSection/>
     </div>
   )
